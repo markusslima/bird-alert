e61526fc63e98f02b9e60836a7be61ef78b8a935
Markus Lima <markusslima@gmail.com>
2018-02-09 10:41:40 -0200

    Add method notify

--- src/bird-alert.js
+++ src/bird-alert.js
@@ -5,23 +5,36 @@
  */
 function BirdAlert(options) {
     "use strict";
+
+    var _this = this;
+
+    this.element = [];
+
     var defaults = {
-        duration: 5000,
-        position: 'top left',
-        notifyLimit: 7,
-        zindex: 1000,
-        onShow: function () {},
-        onHide: function () {}
-    },
-    positions = {
-        'top left': ,
-        'top center':,
-        'top right':,
-        'bottom left':,
-        'bottom center':,
-        'bottom right':,
-    },
-    css = {
+            clickToHide: true,
+            autoHide: true,
+            duration: 5000,
+            className: 'error',
+            showAnimation: 'slideInDown',
+            showDuration: 400,
+            hideAnimation: 'slideOutRight',
+            hideDuration: 200,
+            position: 'top left',
+            quantity: 7,
+            width: 400,
+            zindex: 1000,
+            onShow: function () {},
+            onHide: function () {}
+        },
+        positions = {
+            'top left': {'top': '10px', 'left': '10px', 'bottom': 'auto', 'right': 'auto'},
+            'top center': {'top': '10px', 'left': '50%', 'bottom': 'auto', 'right': 'auto'},
+            'top right': {'top': '10px', 'left': 'auto', 'bottom': 'auto', 'right': '10px'},
+            'bottom left': {'top': 'auto', 'left': '10px', 'bottom': '10px', 'right': 'auto'},
+            'bottom center': {'top': 'auto', 'left': '50%', 'bottom': '10px', 'right': '0'},
+            'bottom right': {'top': 'auto', 'left': 'auto', 'bottom': '10px', 'right': '10px'}
+        };
+    /*css = {
         'position': 'absolute'
     };
 
@@ -47,18 +60,6 @@ function BirdAlert(options) {
 
     };
 
-
-
-
-
-
-
-    function html() {
-    }
-
-
-
-
     function hide() {
         el.removeClass('flipInX').addClass('flipOutX');
         el.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
@@ -75,14 +76,55 @@ function BirdAlert(options) {
         hide();
     }, 7000));
 
-    el.show();
+    el.show();*/
+
+    this.notify = function (opt) {
+        var html = ''
+            + '<div class="container-birdAlert '+(opt.type || "")+'">'
+            + '  <i class="close-birdAlert fa fa-close fa-fw"></i>'
+            + (opt.title ? '  <div class="title-birdAlert">'+opt.title+'</div>' : '')
+            + '  <div class="content-birdAlert">'+opt.msg+'</div>'
+            + '</div>';
+
+
+        if (options.position.search('top') != -1) {
+            _this.element.prepend(html);
+            if (_this.element.find('.container-birdAlert').length >= options.quantity) {
+                _this.element.find('.container-birdAlert:last').remove();
+            }
+
+            if (options.autoHide) {
+                setTimeout(function () {
+                    _this.element.find('.container-birdAlert:last').remove();
+                }, options.duration);
+            }
+        } else {
+            _this.element.append(html);
+            if (_this.element.find('.container-birdAlert').length >= options.quantity) {
+                _this.element.find('.container-birdAlert:first').remove();
+            }
+
+            if (options.autoHide) {
+                setTimeout(function () {
+                    _this.element.find('.container-birdAlert:first').remove();
+                }, options.duration);
+            }
+        }
+    };
 
     // CONSTRUCTOR
     (function () {
-        if ($('#birdAlert').length == 0) {
-            $('body').append('<div id="birdAlert"></div>');
-        }
-
         options = $.extend({}, defaults, options, typeof options === 'object' && options);
+        var len = $('.birdAlert').length+1;
+
+        $('body').append('<div class="birdAlert" id="birdAlert'+len+'"></div>');
+
+        _this.element = $('#birdAlert'+len);
+        _this.element.css(positions[options.position]);
+        _this.element.css({'width': options.width+'px'});
+
+        if (options.position == 'top center' || options.position == 'bottom center') {
+            _this.element.css({'margin-left': '-'+(options.width/2)+'px'});
+        }
     })();
 }
--- test/test.js
+++ test/test.js
@@ -1,11 +1,21 @@
 var birdAlert = new BirdAlert({
-	
+	position: 'top right'
 });
 
 
 
-
-
 $('#btnNotify').on('click', function () {
-	BirdAlert.notify('Mensagem de alerta');
-})
+	birdAlert.notify({
+		msg: 'Mensagem de alerta', 
+		title: 'Success', 
+		type: 'success'
+	});
+});
+
+$('#btnNotifyError').on('click', function () {
+	birdAlert.notify({
+		msg: 'Mensagem de alerta', 
+		title: 'Error', 
+		type: 'error'
+	});
+});
