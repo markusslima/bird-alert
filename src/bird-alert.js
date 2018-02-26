25575d2b2346f9c1c7cb2dec7b7e5729a7d02864
Markus Lima <markusslima@gmail.com>
2018-02-26 11:52:08 -0300

    Adding new methods

--- src/bird-alert.js
+++ src/bird-alert.js
@@ -1,28 +1,37 @@
-/**
- * @description Exibe uma mensagem na tela
- * @function
- * @public
+
+/*
+ * bird-alert
+ * doc: http://markusslima.github.io/bird-alert/
+ * github: https://github.com/markusslima/bird-alert
+ *
+ * Copyright (c) 2018 Markus Vinicius da Silva Lima
+ * Version 1.0.0
+ * Licensed under the MIT license.
  */
 function BirdAlert(options) {
     "use strict";
 
-    this.defaults = {
+    var _this = this,
+
+    defaults = {
         clickToHide: true,
+        closeButton: true,
         autoHide: true,
         duration: 5000,
         className: 'error',
-        showAnimation: 'slideInDown',
-        hideAnimation: 'slideOutRight',
-        position: 'top left',
+        showAnimation: 'flipInX',
+        hideAnimation: 'flipOutX',
+        inverseInsert: false,
+        preventDuplicate: false,
+        position: 'top right',
         quantity: 7,
         agroup: true,
         width: 400,
         zIndex: 1000,
         onShow: function () {},
         onHide: function () {}
-    };
+    },
 
-    var _this = this,
     positions = {
         'top left': {'top': '10px', 'left': '10px', 'bottom': 'auto', 'right': 'auto'},
         'top center': {'top': '10px', 'left': '50%', 'bottom': 'auto', 'right': 'auto'},
@@ -34,15 +43,32 @@ function BirdAlert(options) {
 
     this.element = [];
 
-    this.notify = function (opt) {
-        var el = $('<div class="container-birdAlert animated flipInX '+(opt.type || "")+'">'
-                   + '  <i class="close-birdAlert fa fa-close fa-fw"></i>'
-                   + (opt.title ? '  <div class="title-birdAlert">'+opt.title+'</div>' : '')
-                   + '  <div class="content-birdAlert">'+opt.msg+'</div>'
-                   + '</div>');
+    this.notify = function (param) {
+        var flag = false;
+        if (options.preventDuplicate) {
+            _this.element.find('.container-birdAlert').each(function () {
+                if ($(this).find('.content-birdAlert').html() == param.msg && $(this).hasClass(param.className)) {
+                    flag = true;
+                }
+            });
+        }
+
+        if (flag) {
+            return false;
+        }
+
+        var el = $('<div class="container-birdAlert animated '+options.showAnimation+' '+(param.className || '')+'">' +
+                   (options.closeButton ? '  <i class="close-birdAlert">+</i>' : '') +
+                   (param.title ? '  <div class="title-birdAlert">'+param.title+'</div>' : '') +
+                   '  <div class="content-birdAlert">'+param.msg+'</div>' +
+                   '</div>');
 
         if (options.position.search('top') != -1) {
-            _this.element.prepend(el);
+            if (!options.inverseInsert) {
+                _this.element.prepend(el);
+            } else {
+                _this.element.append(el);
+            }
             if (options.agroup) {
                 if (_this.element.find('.container-birdAlert').length >= options.quantity) {
                     _this.element.find('.container-birdAlert:last').remove();
@@ -75,21 +101,36 @@ function BirdAlert(options) {
             el.on('click', function () {
                 hideElement($(this));
             });
+        } else if (options.closeButton) {
+            el.find('.close-birdAlert').on('click', function () {
+                hideElement(el);
+            });
         }
+
+        options.onShow();
     };
 
     var hideElement = function (el) {
-        el.removeClass('flipInX').addClass('flipOutX');
+        el.removeClass(options.showAnimation).addClass(options.hideAnimation);
         el.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
             el.remove();
         });
         clearTimeout(el.data('timeOut'));
+        options.onHide();
     };
 
-    // CONSTRUCTOR
+    this.set = function (param) {
+        options = $.extend({}, options, param, typeof param === 'object' && param);
+
+        _this.element.css(positions[options.position]);
+        if (options.position == 'top center' || options.position == 'bottom center') {
+            _this.element.css({'margin-left': '-'+(options.width/2)+'px'});
+        }
+        _this.element.css({'z-index': options.zIndex });
+    };
+
     (function () {
         options = $.extend({}, defaults, options, typeof options === 'object' && options);
-        var len = $('.birdAlert').length+1;
         _this.element = $('<div class="birdAlert"></div>');
         $('body').append(_this.element);
         _this.element.css(positions[options.position]);
--- test/test.js
+++ test/test.js
@@ -1,21 +1,49 @@
 var birdAlert = new BirdAlert({
-	position: 'top right',
+	position: 'top right'
 });
 
 
 
-$('#btnNotify').on('click', function () {
+$('#btnSuccess').on('click', function () {
 	birdAlert.notify({
-		msg: 'Mensagem de alerta', 
+		msg: 'Facilisis pellentesque dictumst dignissim, mauris et tincidunt tincidunt', 
 		title: 'Success', 
-		type: 'success'
+		className: 'success'
 	});
 });
 
-$('#btnNotifyError').on('click', function () {
+$('#btnError').on('click', function () {
 	birdAlert.notify({
-		msg: 'Mensagem de alerta', 
+		msg: 'Facilisis pellentesque dictumst dignissim, mauris et tincidunt tincidunt', 
 		title: 'Error', 
-		type: 'error'
+		className: 'error'
+	});
+});
+
+$('#btnWarning').on('click', function () {
+	birdAlert.notify({
+		msg: 'Facilisis pellentesque dictumst dignissim, mauris et tincidunt tincidunt', 
+		title: 'Warning', 
+		className: 'warning'
+	});
+});
+$('#btnInfo').on('click', function () {
+	birdAlert.notify({
+		msg: 'Facilisis pellentesque dictumst dignissim, mauris et tincidunt tincidunt', 
+		title: 'Info', 
+		className: 'info'
+	});
+});
+$('#btnSet').on('click', function () {
+	birdAlert.set({
+		position: 'bottom center',
+		autoHide: false,
+		clickToHide: false
+	});
+
+	birdAlert.notify({
+		msg: 'Facilisis pellentesque dictumst dignissim, mauris et tincidunt tincidunt. Dictumst dignissim, mauris et tincidunt', 
+		title: 'Info', 
+		className: 'info'
 	});
 });
